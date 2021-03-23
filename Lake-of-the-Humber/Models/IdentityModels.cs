a49d4c8e0326a9df8e1b83fff8b66e3e718c76b7
Praveen Ramkumar <praveenramkumar@outlook.com>
2021-03-23 16:02:46 -0400

    Added Info Section Model

--- Lake-of-the-Humber/Models/IdentityModels.cs
+++ Lake-of-the-Humber/Models/IdentityModels.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.Entity;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,6 +11,7 @@ namespace Lake_of_the_Humber.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public ICollection<InfoSection> InfoSection { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -25,6 +28,15 @@ namespace Lake_of_the_Humber.Models
         {
         }
 
+        //Instruction to set the models as tables in our database.
+        /// <summary>
+        /// This is the model that is linked to the associated users of the application
+        /// </summary>
+        public IEnumerable ApplicationUsers { get; internal set; }
+        /// <summary>
+        /// This is the model that is linked to the Information Section of Homepage
+        /// </summary>
+        public DbSet<InfoSection> InfoSections { get; set; }
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
