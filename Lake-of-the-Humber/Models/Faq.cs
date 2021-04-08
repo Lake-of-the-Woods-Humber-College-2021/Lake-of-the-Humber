de3b141face2696c81ff8aac900a0a5f3f9ecf67
asia gault <asialevesquegault@gmail.com>
2021-04-08 14:06:11 -0600

    FAQ BRANCH MERGE

--- Lake-of-the-Humber/Models/Faq.cs
+++ Lake-of-the-Humber/Models/Faq.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.EnterpriseServices.Internal;
 using System.Linq;
 using System.Web;
 
@@ -11,6 +12,13 @@ namespace Lake_of_the_Humber.Models
     public class Faq
 
     {
+        //FAQ ID(PK)
+        //date created
+        //question
+        //answer
+        //Publish
+        //user_id
+        //possible image
     }
 
 }
